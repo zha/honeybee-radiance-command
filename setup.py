32ae4ae2aa62550fb26b3e5fc693372ebfb167f8
Mostapha <sadeghipour@gmail.com>
2020-04-08 20:11:01 -0400

    build(deps-dev): bump dependencies

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-import re
 import setuptools
-import sys
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
@@ -8,11 +6,11 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="honeybee-radiance-command",
-    use_scm_version = True,
+    use_scm_version=True,
     setup_requires=['setuptools_scm'],
     author="Ladybug Tools",
     author_email="info@ladybug.tools",
-    description="Honeybee wrapper around Radiance commands which is used by honeybee-radiance",
+    description="Python wrapper around Radiance commands.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ladybug-tools/honeybee-radiance-command",
@@ -21,6 +19,7 @@ setuptools.setup(
     classifiers=[
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent"
