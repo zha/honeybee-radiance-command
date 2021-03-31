e574214c0ba500c2076afc710f02b5421dd9191d
Devang <devngc@gmail.com>
2021-03-31 23:01:55 +0530

    test(rpict_test): Add rpict test

---
+++ tests/rpict_test.py
@@ -0,0 +1,47 @@
+from honeybee_radiance_command.rpict import Rpict
+import pytest
+import honeybee_radiance_command._exception as exceptions
+
+
+def test_defaults():
+    rpict = Rpict()
+    assert rpict.command == 'rpict'
+    assert rpict.options.to_radiance() == ''
+    with pytest.raises(exceptions.MissingArgumentError):
+        # missing octree
+        rpict.to_radiance()
+
+
+def test_assignment():
+    rpict = Rpict()  
+    rpict.octree = 'input.oct'
+    assert rpict.octree == 'input.oct'
+    rpict.view = 'view.vf'
+    assert rpict.view == 'view.vf'
+    assert rpict.to_radiance() == 'rpict input.oct < view.vf'
+    rpict.output = 'results.dat'
+    assert rpict.output == 'results.dat'
+    assert rpict.to_radiance() == 'rpict input.oct < view.vf > results.dat'
+
+
+def test_stdin():
+    rpict = Rpict()  
+    rpict.octree = 'input.oct'
+    rpict.view = 'view.vf'
+    rpict.output = 'results.dat'
+    assert rpict.to_radiance(stdin_input=True) == 'rpict input.oct > results.dat'
+
+
+def test_validation():
+    rpict = Rpict()
+    with pytest.raises(exceptions.MissingArgumentError):
+        # missing octree
+        rpict.to_radiance()
+
+    rpict.octree = 'input.oct'
+    with pytest.raises(exceptions.MissingArgumentError):
+        # missing sensors
+        rpict.to_radiance()
+
+    rpict.view = 'view.vf'
+    assert rpict.to_radiance() == 'rpict input.oct < view.vf'
