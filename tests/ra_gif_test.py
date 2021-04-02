1f908aca2e5246e0f8fad9932e8fc686ed147634
Devang <devngc@gmail.com>
2021-04-02 23:38:36 +0530

    test(ra_gif_test): Add unit test for ra_gif command

---
+++ tests/ra_gif_test.py
@@ -0,0 +1,37 @@
+from honeybee_radiance_command.ra_gif import Ra_GIF
+import pytest
+import honeybee_radiance_command._exception as exceptions
+
+
+def test_defaults():
+    ra_gif = Ra_GIF()
+    assert ra_gif.command == 'ra_gif'
+    assert ra_gif.options.to_radiance() == ''
+    with pytest.raises(exceptions.MissingArgumentError):
+        ra_gif.to_radiance()
+
+
+def test_assignment():
+    ra_gif = Ra_GIF()
+
+    ra_gif.input = 'image.hdr'
+    assert ra_gif.input == 'image.hdr'
+    assert ra_gif.to_radiance() == 'ra_gif < image.hdr'
+    ra_gif.output = 'results.dat'
+    assert ra_gif.output == 'results.dat'
+    assert ra_gif.to_radiance() == 'ra_gif < image.hdr > results.dat'
+
+
+def test_stdin():
+    ra_gif = Ra_GIF()
+    ra_gif.input = 'image.hdr'
+    ra_gif.output = 'results.dat'
+    assert ra_gif.to_radiance(stdin_input=True) == 'ra_gif > results.dat'
+
+
+def test_validation():
+    ra_gif = Ra_GIF()
+    with pytest.raises(exceptions.MissingArgumentError):
+        ra_gif.to_radiance()
+    ra_gif.input = 'image.hdr'
+    assert ra_gif.to_radiance() == 'ra_gif < image.hdr'
