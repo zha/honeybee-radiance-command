3b86639e7e2d91cff1849bf8ee8ef86364202353
Devang <devngc@gmail.com>
2021-04-02 23:36:03 +0530

    refactor(ra_gif): Added new methods

--- honeybee_radiance_command/ra_gif.py
+++ honeybee_radiance_command/ra_gif.py
@@ -4,11 +4,11 @@
 
 from .options.ra_gif import RaGIFOptions
 from ._command import Command
-import warnings
 import honeybee_radiance_command._typing as typing
+import honeybee_radiance_command._exception as exceptions
 
 
-class RaGIF(Command):
+class Ra_GIF(Command):
     """ra_gif command."""
 
     __slots__ = ('_input')
@@ -16,15 +16,61 @@ class RaGIF(Command):
     def __init__(self, options=None, output=None, input=None):
         Command.__init__(self, output=output)
         self._input = input
-        self._options = options
-    
+        if options:
+            self._options = options
+        else:
+            self._options = RaGIFOptions()
+
     @property
     def options(self):
         """RaGIF options."""
         return self._options
-    
+
     @options.setter
     def options(self, value):
         if not value:
             value = RaGIFOptions()
-    
+
+        if not isinstance(value, RaGIFOptions):
+            raise ValueError('Expected RaGIFoptions not {}'.format(value))
+
+        self._options = value
+
+    @property
+    def input(self):
+        """Radiance HDR image."""
+        return self._input
+
+    @input.setter
+    def input(self, value):
+        if value[-4:] != '.hdr':
+            raise ValueError(
+                'Radiance generated HDR required. Instead got {}'.format(value))
+        else:
+            self._input = typing.normpath(value)
+
+    def to_radiance(self, stdin_input=False):
+        """Command in Radiance format.
+
+        Args:
+            stdin_input: A boolean that indicates if the input for this command comes
+            from stdin. This is for instance the case when you pipe the input
+            from another command (default: False).
+        """
+        self.validate(stdin_input)
+
+        command_parts = [self.command, self.options.to_radiance()]
+        cmd = ' '.join(command_parts)
+        if not stdin_input and self.input:
+            cmd = ' < '.join((cmd, self.input))
+        if self.pipe_to:
+            cmd = ' | '.join((cmd, self.pipe_to.to_radiance(stdin_input=True)))
+        elif self.output:
+            cmd = ' > '.join((cmd, self.output))
+
+        return ' '.join(cmd.split())
+
+    def validate(self, stdin_input=False):
+        Command.validate(self)
+        if not stdin_input and not self.input:
+            raise exceptions.MissingArgumentError(self.command, 'input')
