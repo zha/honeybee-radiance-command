d107625db9125b5744e5074821353cebf75fa36b
Devang <devngc@gmail.com>
2021-04-07 00:24:10 +0530

    test(pcomb_test): add pcomb unit tests

---
+++ tests/pcomb_test.py
@@ -0,0 +1,38 @@
+from honeybee_radiance_command.pcomb import Pcomb
+import pytest
+import honeybee_radiance_command._exception as exceptions
+
+
+def test_defaults():
+    pcomb = Pcomb()
+    assert pcomb.command == 'pcomb'
+    assert pcomb.options.to_radiance() == ''
+    with pytest.raises(exceptions.MissingArgumentError):
+        pcomb.to_radiance()
+
+
+def test_assignment():
+    pcomb = Pcomb()
+
+    pcomb.input = ['image1.hdr', 'image2.hdr']
+    assert pcomb.input == 'image1.hdr image2.hdr'
+    assert pcomb.to_radiance() == 'pcomb < image1.hdr image2.hdr'
+    pcomb.output = 'results.dat'
+    assert pcomb.output == 'results.dat'
+    assert pcomb.to_radiance() == 'pcomb < image1.hdr image2.hdr > results.dat'
+
+
+def test_stdin():
+    pcomb = Pcomb()
+    pcomb.input = ['image1.hdr', 'image2.hdr']
+    pcomb.output = 'results.dat'
+    assert pcomb.to_radiance(stdin_input=True) == 'pcomb > results.dat'
+
+
+def test_validation():
+    pcomb = Pcomb()
+    with pytest.raises(exceptions.MissingArgumentError):
+        pcomb.to_radiance()
+    pcomb.input = ['image1.hdr', 'image2.hdr']
+    assert pcomb.to_radiance() == 'pcomb < image1.hdr image2.hdr'
+    
