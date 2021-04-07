3c84313893b3f216aac444f6d4825bf6b2956806
Devang <devngc@gmail.com>
2021-04-07 22:14:55 +0530

    refactor(ra_gif): update command

--- honeybee_radiance_command/ra_gif.py
+++ honeybee_radiance_command/ra_gif.py
@@ -62,11 +62,11 @@ class Ra_GIF(Command):
         command_parts = [self.command, self.options.to_radiance()]
         cmd = ' '.join(command_parts)
         if not stdin_input and self.input:
-            cmd = ' < '.join((cmd, self.input))
+            cmd = ' '.join((cmd, self.input))
         if self.pipe_to:
             cmd = ' | '.join((cmd, self.pipe_to.to_radiance(stdin_input=True)))
         elif self.output:
-            cmd = ' > '.join((cmd, self.output))
+            cmd = ' '.join((cmd, self.output))
 
         return ' '.join(cmd.split())
 
