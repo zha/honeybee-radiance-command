381e52259638237de8add59eaa585e3d7b9950bf
Devang <devngc@gmail.com>
2021-04-07 23:26:52 +0530

    refactor(pcomb): update command

--- honeybee_radiance_command/pcomb.py
+++ honeybee_radiance_command/pcomb.py
@@ -76,7 +76,7 @@ class Pcomb(Command):
         command_parts = [self.command, self.options.to_radiance()]
         cmd = ' '.join(command_parts)
         if not stdin_input and self.input:
-            cmd = ' < '.join((cmd, self.input))
+            cmd = ' '.join((cmd, self.input))
         if self.pipe_to:
             cmd = ' | '.join((cmd, self.pipe_to.to_radiance(stdin_input=True)))
         elif self.output:
@@ -88,4 +88,5 @@ class Pcomb(Command):
         Command.validate(self)
         if not stdin_input and not self.input:
             raise exceptions.MissingArgumentError(self.command, 'input')
+    
     
