0ccb264ca0d8b215d1480bb4d935f5123becc0db
Devang <devngc@gmail.com>
2021-04-14 18:32:27 +0530

    test(gendaylit_test): add more tests

--- tests/gendaylit_test.py
+++ tests/gendaylit_test.py
@@ -12,7 +12,7 @@ def test_assignment():
     gendaylit = Gendaylit(month=1, day=21, time=23.5)
 
     gendaylit.options.g = 0.1
-    gendaylit.options.O = 1
+    gendaylit.options.O = 2
     gendaylit.options.s = True
     assert gendaylit.month == 1
     assert gendaylit.day == 21
@@ -20,24 +20,34 @@ def test_assignment():
     gendaylit.time_zone = 'EST'
     gendaylit.solar_time = True
     assert gendaylit.input == '+1 21 23:30EST'
-    assert gendaylit.to_radiance() == 'gendaylit -O 1 -g 0.1 -s +1 21 23:30EST'
+    assert gendaylit.to_radiance() == 'gendaylit -O 2 -g 0.1 -s +1 21 23:30EST'
     gendaylit.output = 'test.sky'
     assert gendaylit.output == 'test.sky'
-    assert gendaylit.to_radiance() == 'gendaylit -O 1 -g 0.1 -s +1 21 23:30EST > test.sky'
+    assert gendaylit.to_radiance() == ('gendaylit -O 2 -g 0.1 -s +1 21 23:30EST'
+                                       ' > test.sky')
 
 
 def test_assignment_ang():
     gendaylit = Gendaylit.from_ang((23.33, 45.56))
 
     gendaylit.options.P = (6.3, 0.12)
-    gendaylit.options.W = (840, 135)
-    gendaylit.options.L = (165, 200)
-    assert gendaylit.to_radiance() == ('gendaylit -L 165 200 -P 6.3 0.12'
-                                      ' -W 840 135 -ang 23.33 45.56')
+    assert gendaylit.to_radiance() == ('gendaylit -P 6.3 0.12 -ang 23.33 45.56')
     gendaylit.output = 'ang.sky'
     assert gendaylit.output == 'ang.sky'
-    assert gendaylit.to_radiance() == ('gendaylit -L 165 200 -P 6.3 0.12'
-                                       ' -W 840 135 -ang 23.33 45.56 > ang.sky')
+    assert gendaylit.to_radiance() == ('gendaylit -P 6.3 0.12 -ang 23.33 45.56'
+                                       ' > ang.sky')
+
+
+def test_assignment_not_allowed():
+    # Test assignments of arguments that are not allowed to be assigned concurrently
+    gendaylit = Gendaylit.from_ang((23.33, 45.56))
+
+    try:
+        gendaylit.options.W = (840, 135)
+        gendaylit.options.L = (165, 200)
+        gendaylit.to_radiance()
+    except ValueError:
+        pass
 
 
 def test_stdin():
@@ -51,4 +61,4 @@ def test_stdin():
 
 def test_missing_arguments():
     gendaylit = Gendaylit()
-    assert gendaylit.month == None
+    assert gendaylit.month is None
