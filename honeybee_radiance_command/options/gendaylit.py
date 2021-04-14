2c75ea991c859099e599e6c4286bf1ce1f66a192
Devang <devngc@gmail.com>
2021-04-14 18:04:59 +0530

    refactor(gendaylit): refactor gendaylit options

--- honeybee_radiance_command/options/gendaylit.py
+++ honeybee_radiance_command/options/gendaylit.py
@@ -3,11 +3,8 @@ from .optionbase import (
     OptionCollection,
     BoolOption,
     NumericOption,
-    StringOption,
-    StringOptionJoined,
     IntegerOption,
     TupleOption,
-    FileOption
 )
 import warnings
 
@@ -38,9 +35,9 @@ class GendaylitOptions(OptionCollection):
 
         self._P = TupleOption("P", "Espilon data a.k.a Perez parameters", length=2)
         self._W = TupleOption("W", "Direct normal irradiance and diffuse"
-                              " horizontal irradiance", length=2)
+                              " horizontal irradiance", length=2, numtype=int)
         self._L = TupleOption("L", "Direct normal illuminance and"
-                              " diffuse horizontal illuminance", length=2)
+                              " diffuse horizontal illuminance", length=2, numtype=int)
         self._O = IntegerOption("O", "Output", min_value=0, max_value=2)
         self._s = BoolOption("s", "Generate source description of the sun")
         self._g = NumericOption("g", "Average ground reflectance")
@@ -51,7 +48,7 @@ class GendaylitOptions(OptionCollection):
         self._ang = TupleOption("ang", "Altitude & azimuth", length=2, numtype=float)
         self._on_setattr_check = True
 
-        def _on_setattr(self):
+    def _on_setattr(self):
         """This method executes after setting each new attribute.
         Use this method to add checks that are necessary for OptionCollection.
         """
@@ -61,128 +58,128 @@ class GendaylitOptions(OptionCollection):
                 'Options -a, -o and -m do not apply when -ang is set.'
             )
 
-        @property
-        def P(self):
-            """Espilon data a.k.a Perez parameters"""
-            return self._P
+    @property
+    def P(self):
+        """Espilon data a.k.a Perez parameters"""
+        return self._P
 
-        @P.setter
-        def(self, value):
-            self._P.value = ValueError
+    @P.setter
+    def P(self, value):
+        self._P.value = value
 
-        @property
-        def W(self):
-            """A tuple of direct-normal-irradiance (W/m^2) and
-            diffuse-horizontal-irradiance (W/m^2) values.
-            """
-            return self._W
+    @property
+    def W(self):
+        """A tuple of direct-normal-irradiance (W/m^2) and
+        diffuse-horizontal-irradiance (W/m^2) values.
+        """
+        return self._W
 
-        @W.setter
-        def W(self, value):
-            self._W.value = value
+    @W.setter
+    def W(self, value):
+        self._W.value = value
 
-        @property
-        def L(self):
-            """A tuple of direct-normal-illuminance (lm/m^2) and
-            diffuse-horizontal-illuminance (lm/m^2) values.
-            """
-            return self._L
+    @property
+    def L(self):
+        """A tuple of direct-normal-illuminance (lm/m^2) and
+        diffuse-horizontal-illuminance (lm/m^2) values.
+        """
+        return self._L
 
-        @L.setter
-        def L(self, value):
-            self._L.value = value
+    @L.setter
+    def L(self, value):
+        self._L.value = value
 
-        @property
-        def O(self):
-            """Output
+    @property
+    def O(self):
+        """Output
 
-            A value of 0 will output in W/m^2/sr visible radiation,
-            A value of 1 will output in W/m^2/sr solar radiation,
-            A value of 2 will output in lm/m^2/sr luminance.
-            """
-            return self._O
+        A value of 0 will output in W/m^2/sr visible radiation,
+        A value of 1 will output in W/m^2/sr solar radiation,
+        A value of 2 will output in lm/m^2/sr luminance.
+        """
+        return self._O
 
-        @O.setter
-        def O(self, value):
-            self._O.value = value
+    @O.setter
+    def O(self, value):
+        self._O.value = value
 
-        @property
-        def s(self):
-            """Generate source description of the sun."""
-            return self._s
+    @property
+    def s(self):
+        """Generate source description of the sun."""
+        return self._s
 
-        @s.setter
-        def s(Self, value):
-            self._s.value = value
+    @s.setter
+    def s(self, value):
+        self._s.value = value
 
-        @property
-        def g(self):
-            """Average ground reflectance.
+    @property
+    def g(self):
+        """Average ground reflectance.
 
-            This value is used to compute skyfunc when Dz is negative.
-            """
-            return self._g
+        This value is used to compute skyfunc when Dz is negative.
+        """
+        return self._g
 
-        @g.setter
-        def g(self, value):
-            self._g.value = value
+    @g.setter
+    def g(self, value):
+        self._g.value = value
 
-        @property
-        def a(self):
-            """Site latitude.
+    @property
+    def a(self):
+        """Site latitude.
 
-            The value is site latitude in degrees north.
-            (Use negative angle for south latitude.)
-            This is used in the calculation of sun angle.
-            """
-            return self._a
+        The value is site latitude in degrees north.
+        (Use negative angle for south latitude.)
+        This is used in the calculation of sun angle.
+        """
+        return self._a
 
-        @a.setter
-        def a(self, value):
-            self._a.value = value
+    @a.setter
+    def a(self, value):
+        self._a.value = value
 
-        @property
-        def o(self):
-            """Site longitude.
+    @property
+    def o(self):
+        """Site longitude.
 
-            The value is site longitude in degrees west.
-            (Use negative angle for east longitude.)
-            This is used in the calculation of solar time and sun angle. Be sure to give
-            the corresponding standard meridian also! If solar time is given directly,
-            then this option has no effect.
-            """
-            return self._o
+        The value is site longitude in degrees west.
+        (Use negative angle for east longitude.)
+        This is used in the calculation of solar time and sun angle. Be sure to give
+        the corresponding standard meridian also! If solar time is given directly,
+        then this option has no effect.
+        """
+        return self._o
 
-        @o.setter
-        def o(self, value):
-            self._o.value = value
+    @o.setter
+    def o(self, value):
+        self._o.value = value
 
-        @property
-        def m(self):
-            """Standard meridian.
+    @property
+    def m(self):
+        """Standard meridian.
 
-            The site standard meridian is a value in degrees west of Greenwich.
-            (Use negative angle for east.) This is used in the calculation of solar time.
-            Be sure to give the correct longitude also! If a time zone or solar time is
-            given directly, then this option has no effect.
-            """
-            return self._m
+        The site standard meridian is a value in degrees west of Greenwich.
+        (Use negative angle for east.) This is used in the calculation of solar time.
+        Be sure to give the correct longitude also! If a time zone or solar time is
+        given directly, then this option has no effect.
+        """
+        return self._m
 
-        @m.setter
-        def m(self, value):
-            self._m.value = value
+    @m.setter
+    def m(self, value):
+        self._m.value = value
 
-        @property
-        def ang(self):
-            """Altitude & azimuth
+    @property
+    def ang(self):
+        """Altitude & azimuth
 
-            This option gives the solar angles explicitly. The altitude is measured in
-            degrees above the horizon, and the azimuth is measured in degrees
-            west of south.
-            """
-            return self._ang
+        This option gives the solar angles explicitly. The altitude is measured in
+        degrees above the horizon, and the azimuth is measured in degrees
+        west of south.
+        """
+        return self._ang
 
-        @ang.setter
-        def ang(self, value):
-            self._ang.value = value
+    @ang.setter
+    def ang(self, value):
+        self._ang.value = value
         
