b0732b1b46b13a3cb9e39d54065563fdcffab644
Devang <devngc@gmail.com>
2021-04-15 21:52:30 +0530

    refactor(pcond): update stdin command

--- honeybee_radiance_command/pcond.py
+++ honeybee_radiance_command/pcond.py
@@ -66,7 +66,7 @@ class Pcond(Command):
         cmd = ' '.join(command_parts)
 
         if stdin_input and self.input and self.output:
-            cmd = '%s %s' % (cmd, self.input)
+            cmd = '%s | %s %s >' % (self.input, cmd, self.input)
 
         if not stdin_input and self.input:
             cmd = '%s %s' % (cmd, self.input)
