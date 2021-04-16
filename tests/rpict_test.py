e064a5d0a65f6ca9d77ac1303c9959a06dbda0b2
Devang <devngc@gmail.com>
2021-04-16 13:24:58 +0530

    test(rpict_test): add more tests

--- tests/rpict_test.py
+++ tests/rpict_test.py
@@ -4,16 +4,17 @@ import honeybee_radiance_command._exception as exceptions
 
 
 def test_defaults():
+    """Test command."""
     rpict = Rpict()
+
     assert rpict.command == 'rpict'
     assert rpict.options.to_radiance() == ''
-    with pytest.raises(exceptions.MissingArgumentError):
-        # missing octree
-        rpict.to_radiance()
 
 
 def test_assignment():
-    rpict = Rpict()  
+    """Test assignment."""
+    rpict = Rpict()
+
     rpict.octree = 'input.oct'
     assert rpict.octree == 'input.oct'
     rpict.view = 'view.vf'
@@ -24,16 +25,37 @@ def test_assignment():
     assert rpict.to_radiance() == 'rpict input.oct < view.vf > results.dat'
 
 
+def test_assignment_options():
+    """Test assignment of a few options."""
+    rpict = Rpict()
+
+    rpict.octree = 'input.oct'
+    rpict.view = 'view.vf'
+    rpict.options.vt = 'v'
+    assert rpict.to_radiance() == 'rpict -vtv input.oct < view.vf'
+    with pytest.raises(exceptions.InvalidValueError):
+        rpict.options.vt = 'k'
+    rpict.options.av = (0.0, 0.0, 0.0)
+    assert rpict.to_radiance() == 'rpict -av 0.0 0.0 0.0 -vtv input.oct < view.vf'
+    rpict.options.x = 512
+    assert rpict.to_radiance() == 'rpict -av 0.0 0.0 0.0 -vtv -x 512 input.oct < view.vf'
+
+
 def test_stdin():
-    rpict = Rpict()  
+    """Test stdin."""
+    rpict = Rpict()
+
     rpict.octree = 'input.oct'
     rpict.view = 'view.vf'
     rpict.output = 'results.dat'
-    assert rpict.to_radiance(stdin_input=True) == 'rpict input.oct > results.dat'
+    assert rpict.to_radiance(stdin_input=True) == (
+        'view.vf | rpict input.oct > results.dat')
 
 
 def test_validation():
+    """Test for errors in case of missing arguments."""
     rpict = Rpict()
+
     with pytest.raises(exceptions.MissingArgumentError):
         # missing octree
         rpict.to_radiance()
@@ -42,5 +64,52 @@ def test_validation():
     with pytest.raises(exceptions.MissingArgumentError):
         # missing view file
         rpict.to_radiance()
+
     rpict.view = 'view.vf'
     assert rpict.to_radiance() == 'rpict input.oct < view.vf'
+
+
+def test_error_0():
+    """Test if errors are being raised when incompatible options are assigned."""
+    rpict = Rpict()
+
+    rpict.octree = 'input.oct'
+    rpict.view = 'view.vf'
+    rpict.options.ai = 'mod'
+    with pytest.raises(ValueError):
+        rpict.options.ae = 'mod'
+
+
+def test_error_1():
+    """Test if errors are being raised when incompatible options are assigned."""
+    rpict1 = Rpict()
+
+    rpict1.octree = 'input.oct'
+    rpict1.view = 'view.vf'
+    rpict1.options.aI = 'mod'
+    with pytest.raises(ValueError):
+        rpict1.options.aE = 'mod'
+
+
+def test_warning_0():
+    """Test if warnings are being raised when incompatible options are assigned."""
+    rpict = Rpict()
+
+    rpict.octree = 'input.oct'
+    rpict.view = 'view.vf'
+    rpict.options.ps = -1
+    assert rpict.to_radiance() == 'rpict -ps -1 input.oct < view.vf'
+    with pytest.warns(Warning):
+        rpict.options.dj = 0.0
+
+
+def test_warning_1():
+    """Test if warnings are being raised when incompatible options are assigned."""
+    rpict = Rpict()
+
+    rpict.octree = 'input.oct'
+    rpict.view = 'view.vf'
+    rpict.options.i = True
+    assert rpict.to_radiance() == 'rpict -i input.oct < view.vf'
+    with pytest.warns(Warning):
+        rpict.options.dv = True
