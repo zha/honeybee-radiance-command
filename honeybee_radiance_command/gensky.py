2ffe37e55918fbd846203061fc3291469dd168af
Devang <devngc@gmail.com>
2021-04-28 18:05:36 +0530

    style(gensky): improve documentation

--- honeybee_radiance_command/gensky.py
+++ honeybee_radiance_command/gensky.py
@@ -9,34 +9,43 @@ import honeybee_radiance_command._exception as exceptions
 
 
 class Gensky(Command):
-    """Gensky command."""
+    """Gensky Command.
+
+    Args:
+        month: An integer representing the number of the month. Count starts from 01.
+        day: An integer representing the number of the day in a month. Count starts
+            from 01.
+        time: A string representing hour and minute in 24 hours format.
+            Examples of acceptable format are 21.30 and 21:30.
+        time_zone: A three letter text representing the time zone.
+            Following are acceptable time zones with their corresponding hour
+            differences from Greenwhich mean time;
+            YST +9, PST +8, MST +7, CST +6, EST +5, GMT 0, CET -1, EET -2, AST -3,
+            GST -4, IST -5.5, JST -9, NZST -12, YDT +8, PDT +7, MDT +6, CDT +5,
+            EDT +4, BST -1, CEST -2, EEST -3, ADT -4, GDT -5, IDT -6.5, JDT -10,
+            NZDT -13
+        solar_time: A boolean to use local solar time. If set to True then the time
+            is preceded by '+' sign and local solar time is used instead of local
+            standard time.
+        options: Command options. It will be set to Radiance default values if not
+            provided by user.
+        output: File path to the output file (Default: None).
+
+        Properties
+            *options
+            *month
+            *day
+            *time
+            *time_zone
+            *solar_time
+            *input
+        """
 
     __slots__ = ('_month', '_day', '_time', '_time_zone', '_solar_time', '_input')
 
     def __init__(self, month=None, day=None, time=None, time_zone=None,
                  solar_time=False, options=None, output=None):
-        """Command.
-
-        Args:
-            month: An integer representing the number of the month. Count starts from 01.
-            day: An integer representing the number of the day in a month. Count starts
-                from 01.
-            time: A string representing hour and minute in 24 hours format.
-                Examples of acceptable format are 21.30 and 21:30.
-            time_zone: A three letter text representing the time zone.
-                Following are acceptable time zones with their corresponding hour
-                differences from Greenwhich mean time;
-                YST +9, PST +8, MST +7, CST +6, EST +5, GMT 0, CET -1, EET -2, AST -3,
-                GST -4, IST -5.5, JST -9, NZST -12, YDT +8, PDT +7, MDT +6, CDT +5,
-                EDT +4, BST -1, CEST -2, EEST -3, ADT -4, GDT -5, IDT -6.5, JDT -10,
-                NZDT -13
-            solar_time: A boolean to use local solar time. If set to True then the time
-                is preceded by '+' sign and local solar time is used instead of local
-                standard time.
-            options: Command options. It will be set to Radiance default values if not
-                provided by user.
-            output: File path to the output file (Default: None).
-        """
+        
         Command.__init__(self, output=output)
         self.options = options
         self.month = month
