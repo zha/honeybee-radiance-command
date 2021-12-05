4659c348db8e77e913ab4fb63ab5757f7d10a4fb
Built Tools <mail@built.tools>
2021-12-05 15:09:48 +0100

    test(typing path): Added tests for the typing path checkers.

--- tests/typing_test.py
+++ tests/typing_test.py
@@ -1,12 +1,13 @@
 import os
-from honeybee_radiance_command._typing import normpath
+from honeybee_radiance_command._typing import normpath, path_checker, \
+    path_checker_multiple
 import pytest
+
+test_path1 = 'tests/assets/receiver.rad'
+test_path2 = 'tests/assets/dir with space/Jan 1 12.sky'
 
 
 def test_normpath():
-    test_path1 = 'tests/assets/receiver.rad'
-    test_path2 = 'tests/assets/dir with space/Jan 1 12.sky'
-
     normpath_1 = normpath(test_path1)
     normpath_2 = normpath(test_path2)
 
@@ -22,3 +23,38 @@ def test_normpath():
     assert os.path.exists(normpath_1)
     with pytest.raises(AssertionError):
         assert os.path.exists(normpath_2)
+
+
+def test_path_checker():
+    pth1 = path_checker(test_path1)
+
+    pth2 = path_checker(test_path1, extn_list=['.rad'])
+
+    with pytest.raises(ValueError):
+        pth2 = path_checker(test_path2, extn_list=['.rad', '.mat'])
+
+
+def test_path_checker_multiple():
+    pth_list = path_checker_multiple([test_path1, test_path2])
+    if os.name == 'nt':
+        assert pth_list == ['tests/assets/receiver.rad',
+                            '"tests/assets/dir with space/Jan 1 12.sky"']
+    else:
+        assert pth_list == ['tests/assets/receiver.rad',
+                            "'tests/assets/dir with space/Jan 1 12.sky'"]
+
+    pth_list_str = path_checker_multiple([test_path1, test_path2],
+                                         outputs_as_string=True)
+    if os.name == 'nt':
+        assert pth_list_str == r'tests/assets/receiver.rad ' \
+                               r'"tests/assets/dir with space/Jan 1 12.sky"'
+    else:
+        assert pth_list_str == r'tests/assets/receiver.rad ' \
+                               r"'tests/assets/dir with space/Jan 1 12.sky'"
+
+    pth_list = path_checker_multiple([test_path1, test_path2],
+                                     extn_list=('.rad', '.sky'))
+
+    with pytest.raises(ValueError):
+        pth_list = path_checker_multiple([test_path1, test_path2],
+                                         extn_list=('.rad', '.mat'))
